a32b64c6e56d45a3bad07e156fda03af79d457f3
Aydogdy Agabayev <mr.aydogdy02@gmail.com>
2022-01-03 17:37:11 +0500

    Added bg image change in ServiceSection

--- src/components/Main/ServiceSection.js
+++ src/components/Main/ServiceSection.js
@@ -1,10 +1,24 @@
 // IMPORT MODULES
-import React from "react";
+import React, { useRef } from "react";
 
 // IMPORT GLOBAL COMPONENTS
 import SectionTitle from "../Global/SectionTitle";
 
+// IMPORT IMAGES
+
 const ServiceSection = () => {
+  const img_1 = useRef();
+  const img_2 = useRef();
+  const img_3 = useRef();
+  const img_4 = useRef();
+  const img_5 = useRef();
+
+  const HandleZ = (obj, objAr) => {
+    objAr.forEach((el) => {
+      el.current.style.opacity = "0";
+    });
+    obj.current.style.opacity = "1";
+  };
 
   return (
     <section className="service-section-outer">
@@ -13,7 +27,17 @@ const ServiceSection = () => {
       </div>
       <div className="service-section">
         <div className="service-content">
-          <div className="service-block">
+          <div ref={img_1} className="service-bg img-1"></div>
+          <div ref={img_2} className="service-bg img-2"></div>
+          <div ref={img_3} className="service-bg img-3"></div>
+          <div ref={img_4} className="service-bg img-4"></div>
+          <div ref={img_5} className="service-bg img-5"></div>
+          <div
+            className="service-block"
+            onMouseOver={() =>
+              HandleZ(img_1, [img_1, img_2, img_3, img_4, img_5])
+            }
+          >
             <h3>Проектирование</h3>
             <h6 className="fade-out">
               Мы создаем строгие, стильные, актуальные интерьеры в стиле,
@@ -21,7 +45,12 @@ const ServiceSection = () => {
               классики и современных технологий.
             </h6>
           </div>
-          <div className="service-block">
+          <div
+            className="service-block"
+            onMouseOver={() =>
+              HandleZ(img_2, [img_1, img_2, img_3, img_4, img_5])
+            }
+          >
             <h3>Дизайн</h3>
             <h6 className="fade-out">
               Мы создаем строгие, стильные, актуальные интерьеры в стиле,
@@ -29,7 +58,12 @@ const ServiceSection = () => {
               классики и современных технологий.
             </h6>
           </div>
-          <div className="service-block">
+          <div
+            className="service-block"
+            onMouseOver={() =>
+              HandleZ(img_3, [img_1, img_2, img_3, img_4, img_5])
+            }
+          >
             <h3>Авторский надзор</h3>
             <h6 className="fade-out">
               Мы создаем строгие, стильные, актуальные интерьеры в стиле,
@@ -37,7 +71,12 @@ const ServiceSection = () => {
               классики и современных технологий.
             </h6>
           </div>
-          <div className="service-block">
+          <div
+            className="service-block"
+            onMouseOver={() =>
+              HandleZ(img_4, [img_1, img_2, img_3, img_4, img_5])
+            }
+          >
             <h3>Строительные и отделочные работы</h3>
             <h6 className="fade-out">
               Мы создаем строгие, стильные, актуальные интерьеры в стиле,
@@ -45,7 +84,12 @@ const ServiceSection = () => {
               классики и современных технологий.
             </h6>
           </div>
-          <div className="service-block">
+          <div
+            className="service-block"
+            onMouseOver={() =>
+              HandleZ(img_5, [img_1, img_2, img_3, img_4, img_5])
+            }
+          >
             <h3> Мебель, материалы и элементы декора</h3>
             <h6 className="fade-out">
               Мы создаем строгие, стильные, актуальные интерьеры в стиле,
